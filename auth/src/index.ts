bb5902112e4c733890ec8b84e0b57c8bdc6e1012
sakurawtj <53387760+sakurawtj@users.noreply.github.com>
2023-10-20 17:32:09 -0700

    updated routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
 import {json } from 'body-parser';
 import { currentuserRouter } from './routes/ current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
 
 const app = express();
 app.use(json());
@@ -12,6 +16,16 @@ app.use(currentuserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// app.all('*', async (req, res, next) => {
+//     next(new NotFoundError());
+// })
+app.all('*',  (req, res, next) => {
+    throw new NotFoundError();
+})
+
+app.use(errorHandler);
+
 app.listen(3000, () => {
     console.log('Listening on port 3000!')
 })
