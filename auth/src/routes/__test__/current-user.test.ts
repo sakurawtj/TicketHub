1433fc906bcd58f88e592f388c7e65b588a87168
sakurawtj <53387760+sakurawtj@users.noreply.github.com>
2023-10-25 12:15:45 -0700

    test added

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('response with details about the current user',async () => {
+    // cookies do not exist in the second request in testing, so we store it
+    const cookie = await signin();
+    const response = await request(app)
+        .get('/api/users/currentuser')
+        .set('Cookie', cookie)
+        .send()
+        .expect(200);
+    expect(response.body.currentUser.email).toEqual('test@test.com');
+})
+
+it('response with null if not authenticated', async() => {
+    const response = await request(app)
+        .get('/api/users/currentuser')
+        .send()
+        .expect(200);
+    expect(response.body.currentUser).toEqual(null);
+
+})
