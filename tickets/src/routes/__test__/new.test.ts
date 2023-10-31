f547d43db075de08e7b33fa0b1a9b863745cab37
sakurawtj <53387760+sakurawtj@users.noreply.github.com>
2023-10-31 13:23:05 -0700

    test updated with mock

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import {app} from '../../app';
 import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post request', async () => {
     const response = await request(app)
@@ -75,4 +76,16 @@ it('creates a ticket with valid inputs', async () => {
         .expect(201);
     tickets = await Ticket.find({});
     expect(tickets.length).toEqual(1);
+})
+
+it('publishes an event', async () => {
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'title',
+            price: 20
+        })
+        .expect(201);
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
 })
--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import mongoose from 'mongoose';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('returns 404 if id not exists', async () => {
     const id = new mongoose.Types.ObjectId().toHexString();
@@ -81,4 +82,25 @@ it('updates the ticket successfully', async () => {
         .send();
     expect(ticketResponse.body.title).toEqual("lkjhg")
     expect(ticketResponse.body.price).toEqual(25)
+})
+
+it('publish an event', async () => {
+    const cookie = global.signin();
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: "asdf",
+            price: 20
+        })
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: "lkjhg",
+            price: 25
+        })
+        .expect(200)
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+
 })
